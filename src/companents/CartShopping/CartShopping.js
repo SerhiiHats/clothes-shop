cd2923219e9d384bab77868f6844b5be853eb1db
Serhi Hats <gatsserv@gmail.com>
2023-07-06 00:15:54 +0300

    starting layout popup cart

--- src/companents/CartShopping/CartShopping.js
+++ src/companents/CartShopping/CartShopping.js
@@ -5,11 +5,18 @@ import styles from "./CartShopping.module.scss";
 
 const CartShopping = () => {
   return (
-    <ul>
-      <span className={styles.cartShopping}>
+    <ul className={styles.cartShopping}>
+      <span className={styles.cart}>
         <span><Icon type={iconTypes.cartShopping} size={"30px"} color={"white"}/></span>
         <span>Кошик</span>
       </span>
+
+      <li>fgfdg</li>
+      <li>jjyth</li>
+      <li>QAA</li>
+      <li>QAA</li>
+      <li>QAA</li>
+      <li>QAA</li>
 
     </ul>
   );
