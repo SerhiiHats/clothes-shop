7b30bbffa4c157e94423d47f03b91c5622f5bb3c
Serhi Hats <gatsserv@gmail.com>
2023-07-19 23:58:53 +0300

    fix style for cart

--- src/companents/ItemInCart/ItemInCart.js
+++ src/companents/ItemInCart/ItemInCart.js
@@ -13,10 +13,11 @@ const ItemInCart = ({idProduct, titleProduct, quantity, price, handleCountProduc
           <span onClick={() => handleCountProduct(idProduct, quantity, 0)}><Icon
             type={iconTypes.delete} size={"22px"} color={"#171B1E"}/></span></p>
         <p>{quantity}<span> од.</span></p>
-        <p>{price}<span> грн.</span></p>
-        <p className={styles.wrapTotalPrice}>
-                    <span className={styles.wrapButtons}>
-                      <button className={styles.btnSetCount}
+        {/*<p></p>*/}
+        <div className={styles.wrapTotalPrice}>
+           <span className={styles.price}>{price}<span> грн.</span></span>
+           <span className={styles.wrapButtons}>
+                 <button className={styles.btnSetCount}
                               onClick={() => handleCountProduct(idProduct, quantity, -1)}>-</button>
                       <span className={styles.quantity}>{quantity}</span>
                       <button className={styles.btnSetCount}
@@ -24,7 +25,7 @@ const ItemInCart = ({idProduct, titleProduct, quantity, price, handleCountProduc
                       </span>
           <span
             className={styles.totalPrice}>{Math.round(price * quantity * 100) / 100} грн.</span>
-        </p>
+        </div>
       </div>
       <hr/>
     </li>
